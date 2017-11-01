cd47f011c87b03f938daf432624d15206e660cea
jgx13245 <617214798@qq.com>
2017-11-01 12:16:17 +0800

    2

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+ 		<home-header/>
+ 		<swiper-content/>
+ 	</div>
+</template>
+
+<script>
+import HeaderComponent from "./Header";
+import SwiperComponent from "./Swiper";
+export default {
+	components: {
+		"home-header": HeaderComponent,
+		"swiper-content": SwiperComponent
+	}
+}
+</script>
+
+<style>
+</style>
