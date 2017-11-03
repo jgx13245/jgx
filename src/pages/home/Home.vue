69e50628e42fb78617a9fa67047b08e2290c411f
zhaoyaoluyao <1183711668@qq.com>
2017-11-03 16:15:41 +0800

    list

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,17 @@
 <template>
 	<div>
  		<home-header/>
- 		<swiper-content/>
+ 		<list-content></list-content>
  	</div>
 </template>
 
 <script>
 import HeaderComponent from "./Header";
-import SwiperComponent from "./Swiper";
+import ListComponent from "./List";
 export default {
 	components: {
 		"home-header": HeaderComponent,
-		"swiper-content": SwiperComponent
+		"list-content": ListComponent
 	}
 }
 </script>
---
+++ src/pages/home/List.vue
@@ -0,0 +1,114 @@
+<template>
+ 	<list class="list">
+ 		<h2 class="modList">热销推荐</h2>
+ 		<dl>
+ 			<dt>
+ 				<img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg" alt="故宫">
+ 			</dt>
+ 			<dd>故宫</dd>
+ 			<dd>东方故宫建筑代表，东方故宫建筑东方故宫建筑</dd>
+ 			<dd><span>￥45</span>起</dd>
+ 		</dl>
+ 		<dl>
+ 			<dt>
+ 				<img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg" alt="故宫">
+ 			</dt>
+ 			<dd>故宫</dd>
+ 			<dd>东方故宫建筑代表，东方故宫建筑东方故宫建筑</dd>
+ 			<dd><span>￥45</span>起</dd>
+ 		</dl>
+ 		<dl>
+ 			<dt>
+ 				<img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg" alt="故宫">
+ 			</dt>
+ 			<dd>故宫</dd>
+ 			<dd>东方故宫建筑代表，东方故宫建筑东方故宫建筑</dd>
+ 			<dd><span>￥45</span>起</dd>
+ 		</dl>
+ 		<dl>
+ 			<dt>
+ 				<img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg" alt="故宫">
+ 			</dt>
+ 			<dd>故宫</dd>
+ 			<dd>东方故宫建筑代表，东方故宫建筑东方故宫建筑</dd>
+ 			<dd><span>￥45</span>起</dd>
+ 		</dl>
+ 		<dl>
+ 			<dt>
+ 				<img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg" alt="故宫">
+ 			</dt>
+ 			<dd>故宫</dd>
+ 			<dd>东方故宫建筑代表，东方故宫建筑东方故宫建筑</dd>
+ 			<dd><span>￥45</span>起</dd>
+ 		</dl>
+ 		<dl>
+ 			<dt>
+ 				<img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg" alt="故宫">
+ 			</dt>
+ 			<dd>故宫</dd>
+ 			<dd>东方故宫建筑代表，东方故宫建筑东方故宫建筑</dd>
+ 			<dd><span>￥45</span>起</dd>
+ 		</dl>
+ 		<div class="botList">
+ 			<a>查看所有产品</a>
+ 		</div>
+ 	</list>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+.list{
+	width: 100%;
+}
+.modList{
+	height: .8rem;
+	padding-left: .26rem;
+	line-height: .8rem;
+	color: #212121;
+	background: #ccc;
+}
+.list dl{
+	height: 1.4rem;
+	padding: .24rem;
+	border-bottom: 1px solid #ccc;
+}
+.list dd{
+	float: left;
+	overflow: hidden;
+	height: .4rem;
+	line-height: .4rem;
+	width: 4.08rem;
+	color: #9e9e9e;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+}
+.list dd span{
+	color: #ff8300;
+}
+.list dt{
+	float: left;
+	width: 1.4rem;
+	height: 1.4rem;
+	padding-right: .24rem;
+}
+.list dt img{
+	display: block;
+	width: 1.4rem;
+	height: 1.4rem;
+}
+.botList{
+	height: .88rem;
+}
+.botList a{
+	height: .88rem;
+	display: block;
+	width: 100%;
+	line-height: .88rem;
+	text-align: center;
+}
+</style>
