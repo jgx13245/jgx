02578d87ae47af843000f02c623da043d26e33b1
bxk123 <2114319576@qq.com>
2017-11-04 19:52:59 +0800

    de

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+
 import ListClassify from '@/pages/listClassify/ListClassify'
+
+
+import List from '@/pages/listHot/list-Home'
+import listCity from '@/pages/listHot/List-city'
 
 Vue.use(Router)
 
@@ -12,10 +17,23 @@ export default new Router({
       name: 'home',
       component: Home
     },{
+
       path:'/listClassify',
       name:'listClassify',
       component:ListClassify
+    },
+    {
+      path:'/listHot',
+      name:'listHot',
+      component:List,
+      children:[{
+        path:'a',
+        component:listCity
+      }]
     }
+  
+  
+  
   ]
 })
 
