12863f2ef57b7c71fea258e4e0aa00089b59a26c
bxk123 <2114319576@qq.com>
2017-11-04 22:32:53 +0800

    dsjdsh

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
-<<<<<<< HEAD
 
 import ListClassify from '@/pages/listClassify/ListClassify'
 
 
 import List from '@/pages/listHot/list-Home'
 import listCity from '@/pages/listHot/List-city'
-=======
-import List from '@/pages/listgogong/List'
->>>>>>> 20171104list
+
+import Listgugong from '@/pages/listgogong/List'
+
 
 Vue.use(Router)
 
@@ -20,9 +19,8 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },{
-<<<<<<< HEAD
-
+    },
+    {
       path:'/listClassify',
       name:'listClassify',
       component:ListClassify
@@ -35,16 +33,12 @@ export default new Router({
         path:'a',
         component:listCity
       }]
-
-=======
+    },
+    {
       path:'/listgogong',
       name:'List',
-      component:List
->>>>>>> 20171104list
+      component:Listgugong
     }
-  
-  
-  
   ]
 })
 
