96b364cfa171deace434a44dc193314800f1fa7f
冀光鑫 <>
2018-07-19 14:25:01 +0800

    商户

---
+++ src/components/shop/third.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        第三块组件
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/shop/shop.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        商户页面
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
